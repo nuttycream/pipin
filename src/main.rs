21e01ef3976f21e93c0fe88728f5856f932ccad1
nuttyCream <issanutty@gmail.com>
2026-01-16 07:57:30 -0800

    chore(tui)!: remove TUI and old CLI entry points

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,1 @@
-pub mod args;
-pub mod tui;
-
-use color_eyre::Result;
-
-#[tokio::main]
-async fn main() -> Result<()> {
-    let opts = args::opts().run();
-
-    match opts.command {
-        args::Command::Tui => tui::ui::run(),
-        args::Command::Web => return Ok(()),
-    }
-}
+fn main() {}
